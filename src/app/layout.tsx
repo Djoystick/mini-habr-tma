5b36cb8b6d49e24c9fe1578f4319b47554521606
Djoystick <djoystick@example.com>
2026-02-14 17:56:15 +0300

    fix init-data page window error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
 // app/layout.tsx
-import './globals.css';
-import type { ReactNode } from 'react';
-import { TelegramDebugMountWrapper } from '@/components/TelegramDebugMountWrapper';
+import './globals.css'
+import type { ReactNode } from 'react'
+import Script from 'next/script'
+import { TelegramDebugMountWrapper } from '@/components/TelegramDebugMountWrapper'
 
 export const metadata = {
   title: 'MiniHabr — Telegram Mini App',
   description: 'Простейший Habr-подобный Telegram Mini App',
-};
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ru" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body>
         {children}
         <TelegramDebugMountWrapper />
       </body>
     </html>
-  );
+  )
 }
